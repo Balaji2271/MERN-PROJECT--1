16fc8c4e3f4260b40bfd3ecc465fc2de7feff375
Balaji <105645168+Balaji2271@users.noreply.github.com>
2022-10-17 17:02:37 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
+const cors = require("cors"); 
 
 app.use(express.json());
-
+app.use(cors())
 
 mongoose.connect(
     "mongodb+srv://Balaji:bala2271@cluster0.wltmm9c.mongodb.net/MERN?retryWrites=true&w=majority"
@@ -21,6 +22,13 @@ app.get("/getUsers", (req, res) => {
   });
 });
 
+app.post("/createusers", async (req,res)=>{
+  const user = req.body
+  const newUser = new UserModel(user)
+  await newUser.save();
+
+  res.json(user)
+})
 
 
 app.listen(3001, () => {
